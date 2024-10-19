e1b87a56dda8b48504f6930f2bc9a4f733941c5d
tymoneq <tymontumialis@gmail.com>
2024-10-19 09:20:03 +0200

    kasyno prawie dziala dla n = 1e6

---
+++ Kasyno/kas.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+#include "kaslib.h"
+
+using namespace std;
+
+typedef long long ll;
+
+inline bool bin_search(vector<ll> &used, ll X)
+{
+
+    int lo = 0, hi = used.size() - 1, mid;
+
+    while (lo <= hi)
+    {
+        mid = lo + (hi - lo) / 2;
+        if (used[mid] == X)
+            return 1;
+
+        if (used[mid] < X)
+            lo = mid + 1;
+        else
+            hi = mid - 1;
+    }
+
+    return 0;
+}
+
+inline bool prime(ll n, vector<ll> &used)
+{
+    for (ll i = 2; i * i <= n; i++)
+        if (n % i == 0 && bin_search(used, i))
+            return 0;
+
+    return 1;
+}
+
+int main()
+{
+
+    while (1)
+    {
+
+        ll N = DajN();
+        vector<ll> checked;
+        ll curent_x = 1;
+
+        for (ll i = 2; i <= (N / curent_x); i++)
+            if (prime(i, checked))
+            {
+                ll tmp = Pytaj(i);
+                checked.push_back(i);
+                if (tmp == i)
+                {
+                    curent_x *= i;
+                    ll next_X = i;
+                    while (next_X <= (N / i))
+                    {
+                        next_X *= i;
+                        tmp = Pytaj(next_X);
+                        if (next_X != tmp)
+                            break;
+
+                        curent_x *= i;
+                    }
+                }
+            }
+
+        Odpowiedz(curent_x);
+    }
+}
--- Kasyno/kaslib.h
+++ Kasyno/kaslib.h
@@ -2,51 +2,62 @@
 
 using namespace std;
 
-namespace {
-// Można zmienić te wartości.
-long long N = 1'000'000'000'000'000'000ll;
-int bajtalary = 10'000'000;
+namespace
+{
+    // Można zmienić te wartości.
+    // long long N = 1'000'000'000'000'000'000ll;
+    long long N = 1'000'000ll;
+    int bajtalary = 10'000'000;
 
-// Aby zmienić losowość należy zamienić 1 na dowolną inną liczbę typu int.
-mt19937_64 rng(1);
+    // Aby zmienić losowość należy zamienić 1 na dowolną inną liczbę typu int.
+    mt19937_64 rng(1);
 
-long long X;
-bool czy_zainicjalizowany = false;
-int wygrane = 0;
+    long long X;
+    bool czy_zainicjalizowany = false;
+    int wygrane = 0;
 
-long long wylosuj_liczbe() {
-    return uniform_int_distribution<long long>(1, N)(rng);
-}
+    long long wylosuj_liczbe()
+    {
+        return uniform_int_distribution<long long>(1, N)(rng);
+    }
 
-void inicjalizuj_gre() {
-    X = wylosuj_liczbe();
-}
+    void inicjalizuj_gre()
+    {
+        X = wylosuj_liczbe();
+    }
 
-void inicjalizuj_interakcje() {
-    if (czy_zainicjalizowany) return;
-    inicjalizuj_gre();
-    czy_zainicjalizowany = true;
-}
+    void inicjalizuj_interakcje()
+    {
+        if (czy_zainicjalizowany)
+            return;
+        inicjalizuj_gre();
+        czy_zainicjalizowany = true;
+    }
 
-void proba_zakonczenia() {
-    if (!bajtalary) {
-    	cout << "Liczba wygranych: " << to_string(wygrane) << endl;
-    	exit(0);
+    void proba_zakonczenia()
+    {
+        if (!bajtalary)
+        {
+            cout << "Liczba wygranych: " << to_string(wygrane) << endl;
+            exit(0);
+        }
+    }
+
+    void zjedz_bajtalara()
+    {
+        proba_zakonczenia();
+        --bajtalary;
     }
 }
 
-void zjedz_bajtalara() {
-    proba_zakonczenia();
-    --bajtalary;
-}
-}
-
-long long DajN() {
+long long DajN()
+{
     inicjalizuj_interakcje();
     return N;
 }
 
-long long Pytaj(long long y) {
+long long Pytaj(long long y)
+{
     inicjalizuj_interakcje();
     assert(1 <= y && y <= N);
     zjedz_bajtalara();
@@ -54,14 +65,16 @@ long long Pytaj(long long y) {
     return __gcd(X, y);
 }
 
-void Szturchnij() {
+void Szturchnij()
+{
     inicjalizuj_interakcje();
     zjedz_bajtalara();
     X = wylosuj_liczbe();
     proba_zakonczenia();
 }
 
-void Odpowiedz(long long y) {
+void Odpowiedz(long long y)
+{
     inicjalizuj_interakcje();
     assert(1 <= y && y <= N);
     zjedz_bajtalara();
