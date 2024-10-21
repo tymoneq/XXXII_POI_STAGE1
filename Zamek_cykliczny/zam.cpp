947adb5473f91b24c8faa37324faf7c29c3fde14
tymoneq <tymontumialis@gmail.com>
2024-10-21 14:44:26 +0200

    faf

--- Zamek_cykliczny/zam.cpp
+++ Zamek_cykliczny/zam.cpp
@@ -4,9 +4,6 @@ using namespace std;
 
 typedef long long ll;
 
-constexpr int N = 1e6 + 10;
-ll CostFromFront[N];
-
 int main()
 {
     ios_base::sync_with_stdio(0);
@@ -22,74 +19,63 @@ int main()
         cout << "0\n";
         return 0;
     }
-    ll res = 0;
-
-    CostFromFront[0] = 9 - (s[0] - '0') + 1;
-
-    for (int i = 1; i < s.size(); i++)
+    if (s[0] == '1')
     {
-        CostFromFront[i] = CostFromFront[i - 1];
-        if (s[i] != '0')
-            CostFromFront[i] += 1 + 9 - (s[i] - '0');
-    }
-    ll j = 1, cost = 0;
-    int mxIndx = -1;
-    if (s.size() > 10)
-        for (int i = s.size() - 1; i >= 0; i--)
-        {
-
-            if (cost + (9 - (s[i] - '0')) * j < CostFromFront[i])
-            {
-                mxIndx = i;
-                cost += (9 - (s[i] - '0')) * j;
-                j *= 10;
-            }
-            else
-                break;
-
-            if (j > 10e9)
-                break;
-        }
-
-    res = cost;
-    bool moved = 0;
-    if (mxIndx == -1)
-    {
-        bool XD = ((s.size() > 1 && s[0] == '1') ? 1 : 0);
+        bool XD = 1;
         for (int i = 1; i < s.size(); i++)
             if (s[i] != '0')
             {
                 XD = 0;
                 break;
             }
-
-        if (!XD)
+        if (XD)
         {
-            mxIndx = s.size() - 1;
-            res += (s[s.size() - 1] == '0' ? 0 : 9 - (s[s.size() - 1] - '0'));
+            cout << "1\n";
+            return 0;
         }
-        if (s[s.size() - 1] == '0')
-            moved = 1;
     }
-    for (int i = mxIndx - 1; i >= 0; i--)
+    if (s.size() == 1)
     {
-        if (s[i] == '0')
-        {
-            moved = 1;
-            continue;
-        }
-        if (!moved && s[i] == '9')
-            continue;
-
-        res += 1 + 9 - (s[i] - '0');
-        moved = 1;
+        cout << 10 - (s[0] - '0') + 1 << "\n";
+        return 0;
     }
-    if (mxIndx != -1)
-        res += 2;
-    else
-        res = 1;
+    ll res = numeric_limits<ll>::max();
 
+    int mnToCheck = min((int)s.size(), 9);
+
+    for (int k = 0; k < mnToCheck; k++)
+    {
+        ll cost_of_update_last_k_elements = 0;
+        ll j = 1;
+        for (int i = 0; i < k; i++)
+        {
+            cost_of_update_last_k_elements += (9 - (s[s.size() - i - 1] - '0')) * j;
+            j *= 10;
+        }
+
+        bool moved = 0;
+        for (int i = s.size() - k - 1; i >= 0; i--)
+        {
+            if (s[i] == '0')
+            {
+                moved = 1;
+                continue;
+            }
+            if (s[i] == '9' && moved)
+            {
+                cost_of_update_last_k_elements++;
+                continue;
+            }
+            if (s[i] == '9' && !moved)
+                continue;
+
+            moved = 1;
+            cost_of_update_last_k_elements += 1 + (9 - (s[i] - '0'));
+        }
+
+        res = min(res, cost_of_update_last_k_elements);
+    }
+    res += 2;
     cout << res << "\n";
-
     return 0;
 }
